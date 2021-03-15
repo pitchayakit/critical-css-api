874b45c9a7e0caf51fa1857c4d8c03c8b6f0a97e
Pitchayakit <pitchayakit.p@gmail.com>
2021-03-15 14:53:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const critical = require('critical')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+  //res.send(req.query)
+  let url = req.query.url
+  critical.generate({
+    inline: false,
+    base: 'public/',
+    src: url,
+    dest: './critical.css',
+    dimensions: [
+      {
+        height: 500,
+        width: 300,
+      },
+      {
+        height: 720,
+        width: 1280,
+      },
+    ],
+  }, (err, output) => {
+    if (err) {
+      res.status(400);
+      res.send('None shall pass');
+    } else if (output) {
+      res.send(output)
+    }
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})

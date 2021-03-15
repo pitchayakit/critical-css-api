6260bd7c567f3a3d861f2a232edc5d9271259145
Pitchayakit <pitchayakit.p@gmail.com>
2021-03-15 15:48:56 +0700

    port ischanged

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const critical = require('critical')
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
 
 app.get('/', function (req, res) {
   res.send("HELLO")

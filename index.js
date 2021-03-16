efcc391a2316be3f7b0aa6b96c0d29d5a29243d8
Pitchayakit <pitchayakit.p@gmail.com>
2021-03-16 09:15:37 +0700

    Remove unuse code

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const critical = require('critical')
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 80
 
 app.get('/', function (req, res) {
-  res.send("HELLO")
-  // let url = req.query.url
-  // critical.generate({
-  //   inline: false,
-  //   base: 'public/',
-  //   src: url,
-  //   dest: './critical.css',
-  //   dimensions: [
-  //     {
-  //       height: 500,
-  //       width: 300,
-  //     },
-  //     {
-  //       height: 720,
-  //       width: 1280,
-  //     },
-  //   ],
-  // }, (err, output) => {
-  //   if (err) {
-  //     res.status(400);
-  //     res.send('None shall pass');
-  //   } else if (output) {
-  //     res.send(output)
-  //   }
-  // });
+  let url = req.query.url
+  critical.generate({
+    inline: false,
+    base: 'public/',
+    src: url,
+    dest: './critical.css',
+    dimensions: [
+      {
+        height: 500,
+        width: 300,
+      },
+      {
+        height: 720,
+        width: 1280,
+      },
+    ],
+  }, (err, output) => {
+    if (err) {
+      res.status(400);
+      res.send('None shall pass');
+    } else if (output) {
+      res.send(output)
+    }
+  });
 })
 
 app.listen(port, () => {
